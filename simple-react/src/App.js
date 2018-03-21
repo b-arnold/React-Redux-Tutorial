caba91a53ca23884dca77effca9ae77597f55721
Brett Arnold <brett@Brett-Arnolds-MacBook-Pro.local>
2018-03-20 23:13:40 -0700

    Update 3/20/18

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -14,20 +15,34 @@ class App extends Component {
         super(props);
 
         this.state = {
-            videos: []
-        }
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
-                this.setState({ videos });
-        });
+        this.videoSearch('surfbords')
+    
     }
 
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term }, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+        });
+    };
+
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) } 
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
